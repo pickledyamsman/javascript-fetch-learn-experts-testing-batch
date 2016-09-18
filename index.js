bd106c8b86404fc13f29906d93d7909c0257f6a2
Matthew <pickledyamsman@gmail.com>
2016-09-18 14:27:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-const app = "I don't do much.";
+const token = '4a7799eb4863aeaa6a949dac4f26cd8e4dddfe72'
+fetch('https://api.github.com/pickledyamsman/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json))
